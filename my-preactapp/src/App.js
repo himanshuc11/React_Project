bbb8033e1f2fb5996af67d40bf17f03ed45c1e24
unknown <bandalshubham401@gmail.com>
2021-12-11 16:57:12 +0530

    First

---
+++ my-preactapp/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import Home from "./Home";
+// import { Switch, Route } from "react-router-dom";
+ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+ import Login from "./login/login"
+import Register from "./register/register"
+import Homepage from "./homepage/homepage"
+
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Service from "./Pages/Service";
+import Error from "./Pages/Error";
+
+
+const App = () => {
+  const [ user, setLoginUser] = useState({})
+  return (
+    <>
+   <Router>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/contact" component={Contact}></Route>
+        <Route path="/services" component={Service}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/register" component={Register}></Route>
+        <Route exact path="/">
+            {
+              user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>
+            }
+          </Route>
+          <Login setLoginUser={setLoginUser}/>
+        
+        <Route component={Error}></Route>
+      </Switch>
+      </Router>
+      
+    </>
+  );
+};
+
+export default App;
